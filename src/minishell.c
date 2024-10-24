96ec4d8a7a6b6e0ca13a9535e48f3cd2bec4cbcf
Nader GHADDAR <nghaddar@student.42.fr>
2024-10-24 17:22:16 +0200

    refactor: modified main to be a simple template for testing purposes

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nghaddar <nghaddar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:47:28 by fflamion          #+#    #+#             */
-/*   Updated: 2024/10/24 17:13:25 by nghaddar         ###   ########.fr       */
+/*   Updated: 2024/10/24 17:21:55 by nghaddar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,8 @@
 int	main(int argc, char **argv, char **envp)
 {
 	// initialize_shell();
-	char *rl;
-	char *cwd = malloc(256);
-
-	while (1){
-		rl = readline("minishell>");
-		builtin_cd(rl);
-		cwd = getcwd(cwd, 256);
-		printf("Current working directory : %s\n", cwd);
+	while (42){
+		readline("minishell>");
 	}
 	return (0);
 }
