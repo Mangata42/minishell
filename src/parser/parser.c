1d2bb021499ca91c5f1f11a4ea0088dd89088e4b
Nader Ghr <mangata@Naders-MacBook-Pro.local>
2024-10-30 22:32:05 +0100

    simplification du parser pour ne gerer que les erreurs syntaxiques

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,93 +6,11 @@
 /*   By: nghaddar <nghaddar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:48:46 by nghaddar          #+#    #+#             */
-/*   Updated: 2024/10/30 16:33:20 by nghaddar         ###   ########.fr       */
+/*   Updated: 2024/10/30 22:27:23 by nghaddar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-// Should verify command or not??
-char	*findCommandLocation(char *cmd)
-{
-	int		execPathIndex = 0;
-	char	**execPath;
-	char	*execLocation = NULL;
-	char	*cmdWithSlash;
-	char	*cmdWithPath;
-	char 	*envPath;
-
-	cmdWithSlash = ft_strpreppend(cmd, "/");
-	envPath = getenv("PATH");
-	execPath = ft_split(envPath, ':');
-	while (execPath[execPathIndex])
-	{
-		cmdWithPath = ft_strpreppend(cmdWithSlash, execPath[execPathIndex]);
-		if (access(cmdWithPath, F_OK) == 0)
-			break;
-
-		execPathIndex++;
-		free(cmdWithPath);
-		cmdWithPath = NULL;
-	}
-
-	for (int i = 0; execPath[i]; i++)
-		free(execPath[i]);
-	
-	free(execPath);
-	free(cmdWithSlash);
-	if (cmdWithPath)
-		return (cmdWithPath);
-		
-	return (NULL);
-}
-
-
-int		verif_cmd(t_token *cmd_token)
-{
-	/*TODO : add the condition REDIR_IN -> FILE -> CMD */
-	t_token_type	p_type;
-	int				check_mask;
-
-	if (!cmd_token->index)
-		return (0);
-
-	check_mask = (TOKEN_AND | TOKEN_OR | TOKEN_HEREDOC | TOKEN_PIPE);
-	p_type = cmd_token->prev->type;
-	if (!(check_mask & p_type)){
-		printf("%s: parsing error\n", cmd_token->value);
-		return (1);
-	}
-}
-
-int		verif_args_set(t_token *args_token)
-{
-	t_token_type	p_type;
-	int				variable_check_mask;
-
-	if (!args_token->index)
-	{
-		printf("%s: parsing error\n", args_token->value);
-		return (1);
-	}
-	
-	p_type = args_token->prev->type;
-	variable_check_mask = (TOKEN_PIPE | TOKEN_REDIRECTION_OUT | TOKEN_APPEND
-							| TOKEN_COMMAND);
-	if (args_token->type & (TOKEN_STRING | TOKEN_EXPAND | TOKEN_ARGUMENT 
-							| TOKEN_WILDCARDS))
-		if (p_type != TOKEN_COMMAND){
-			printf("%s: parsing error\n", args_token->value);
-			return (1);
-		}
-		
-	if (args_token->type == TOKEN_VARIABLE && !(variable_check_mask & p_type)){
-		printf("%s: parsing error\n", args_token->value);
-		return (1);
-	}
-
-	return (0);
-}
 
 int		verif_pipe(t_token *pipe_token)
 {
@@ -101,41 +19,32 @@ int		verif_pipe(t_token *pipe_token)
 
 	if (!pipe_token->index)
 	{
-		printf("pipe parsing error\n");
+		printf("minishell: syntax error near unexpected token `|'\n");
 		return (1);
 	}
 
 	p_type = pipe_token->prev->type;
 	check_mask = (TOKEN_COMMAND | TOKEN_VARIABLE);
-
-	if (!(check_mask & p_type))
-	{
-		printf("pipe parsing error\n");
-		return (1);
-	}
-	
+		
 	return (0);
 }
 
 int		verif_redir_in_set(t_token *redir_in_token)
 {
-	if (!redir_in_token->index == 0)
-		return (1);
-	return (0);
-}
-
-int		verif_redir_out_set(t_token *redir_out_token)
-{
-	t_token_type p_type;
+	t_token_type n_type;
 	int			 check_mask;
-
-	p_type = redir_out_token->type;
-	check_mask = (TOKEN_ARGUMENT | TOKEN_STRING | TOKEN_WILDCARDS | TOKEN_EXPAND
-					| TOKEN_COMMAND | TOKEN_VARIABLE | TOKEN_HEREDOC);
 	
-	if (!redir_out_token->index || !(p_type & check_mask))
+	if (!redir_in_token->next)
 	{
-		printf("redirection out parse error\n");
+		printf("minishell: syntax error near unexpected token `newline'\n");
+		return (1);
+	}
+
+	n_type = redir_in_token->next->type;
+	check_mask = TOKEN_STRING | TOKEN_COMMAND | TOKEN_EXPAND;
+	if (!(n_type & check_mask))
+	{
+		printf("minishell: syntax error near unexpected token `%s'\n", redir_in_token->next->value);
 		return (1);
 	}
 	return (0);
@@ -148,7 +57,7 @@ int		verif_operand_set(t_token *operand_token)
 	
 	if (!operand_token->index)
 	{
-		printf("operand parse error\n");
+		printf("minishell: syntax error near unexpected token `%s'\n", operand_token->value);
 		return (1);
 	}
 	
@@ -157,7 +66,7 @@ int		verif_operand_set(t_token *operand_token)
 					| TOKEN_COMMAND); 
 	if (!(p_type & check_mask))
 	{
-		printf("operand parse error\n");
+		printf("minishell: syntax error near unexpected token `%s'\n", operand_token->value);
 		return (1);
 	}
 	
@@ -171,17 +80,11 @@ int	parser(t_t_list *token_list)
 
 	while (token_cursor)
 	{
-		if (token_cursor->type == TOKEN_COMMAND)
-			status = verif_cmd(token_cursor);
-		else if (token_cursor->type & (TOKEN_ARGUMENT |TOKEN_VARIABLE
-		 | TOKEN_STRING | TOKEN_EXPAND | TOKEN_WILDCARDS))
-			status = verif_args_set(token_cursor);
-		else if (token_cursor->type == TOKEN_PIPE)
+		if (token_cursor->type == TOKEN_PIPE)
 			status = verif_pipe(token_cursor);
-		else if (token_cursor->type & (TOKEN_REDIRECTION_IN | TOKEN_HEREDOC))
+		else if (token_cursor->type & (TOKEN_REDIRECTION_IN | TOKEN_HEREDOC 
+										| TOKEN_REDIRECTION_OUT | TOKEN_APPEND))
 			status = verif_redir_in_set(token_cursor);
-		else if (token_cursor->type & (TOKEN_REDIRECTION_OUT | TOKEN_APPEND))
-			status = verif_redir_out_set(token_cursor);
 		else if (token_cursor->type & (TOKEN_AND | TOKEN_OR))
 			status = verif_operand_set(token_cursor);
 		if (status)
