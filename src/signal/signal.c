3488dc12e949f418d7afc539421b8d87243f333f
Florent Flamion <fflamion@paul-f4Ar1s5.clusters.42paris.fr>
2024-10-30 10:52:20 +0100

    ajout system signal

---
+++ src/signal/signal.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fflamion <fflamion@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 09:34:38 by fflamion          #+#    #+#             */
+/*   Updated: 2024/10/30 10:49:12 by fflamion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	handle_sigint(int signum)
+{
+	(void)signum;
+	write(1, "\nminishell> ", 12);
+}
+
+void	setup_signals(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = handle_sigint;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &sa, NULL);
+}
