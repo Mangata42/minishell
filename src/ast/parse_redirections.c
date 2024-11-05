92ae7b548477fd48c1f0b9ece67197c38dd83d9f
= <fflamion@paul-f4Br7s2.clusters.42paris.fr>
2024-11-05 21:52:12 +0100

    les redirections ne sont pas encore opperationelles mais ca avance git add *

--- src/ast/parse_redirections.c
+++ src/ast/parse_redirections.c
@@ -29,25 +29,41 @@ void	parse_redir(t_token **curr_tok, t_ast_node *cmd_node, t_sh *shell)
 	{
 		token = *curr_tok;
 		*curr_tok = (*curr_tok)->next;
-		if (!*curr_tok || ((*curr_tok)->type != TOKEN_ARGUMENT
-			&& (*curr_tok)->type != TOKEN_HEREDOC))
+		if (!*curr_tok ||
+			((*curr_tok)->type != TOKEN_ARGUMENT
+				&& (*curr_tok)->type != TOKEN_HEREDOC
+				&& (*curr_tok)->type != TOKEN_STRING // Ajoutez cette condition
+				&& (*curr_tok)->prev->type != TOKEN_ARGUMENT))
 		{
-			if (*curr_tok != NULL) {
+			if (*curr_tok != NULL)
 				printf("minishell: %s: Aucun fichier ou dossier de ce type\n",
 					(*curr_tok)->value);
-			} else {
+			else
 				printf("minishell: Aucun fichier ou dossier de ce type\n");
-			}
+					// TODO: je ne sais pas si on garde cette erreur
 			shell->exit_status = 1;
 			return ;
 		}
-		redir_node = create_ast_node(AST_REDIRECTION_IN, shell);
+		if (token->type == TOKEN_REDIRECTION_IN)
+		{
+			printf("\n\nredirection in\n\n");
+			redir_node = create_ast_node(AST_REDIRECTION_IN, shell);
+		}
 		if (token->type == TOKEN_REDIRECTION_OUT)
+		{
+			printf("\n\nredirection out\n\n");
 			redir_node = create_ast_node(AST_REDIRECTION_OUT, shell);
+		}
 		else if (token->type == TOKEN_APPEND)
+		{
+			printf("\n\nredirection append\n\n");
 			redir_node = create_ast_node(AST_REDIRECTION_APPEND, shell);
+		}
 		else if (token->type == TOKEN_HEREDOC)
+		{
+			printf("\n\nredirection heredoc\n\n");
 			redir_node = create_ast_node(AST_REDIRECTION_HEREDOC, shell);
+		}
 		redir_node->filename = ft_strdup((*curr_tok)->value);
 		*curr_tok = (*curr_tok)->next;
 		if (!cmd_node->left)
