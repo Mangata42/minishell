80aefa1798a7d395597644a58b3981b099eef6b0
nghaddar <mangata@Naders-MacBook-Pro.local>
2024-11-07 20:27:01 +0100

    fix an issue where heredoc would not throw an error eventhough the next argument was an empty string

--- src/lexer/utils1.c
+++ src/lexer/utils1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils1.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fflamion <fflamion@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nghaddar <nghaddar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:04:19 by fflamion          #+#    #+#             */
-/*   Updated: 2024/11/02 20:02:11 by fflamion         ###   ########.fr       */
+/*   Updated: 2024/11/07 20:23:02 by nghaddar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,8 @@ void	h_rin(char *input, uint16_t *i, t_t_list *t_list)
 		while (input[*i] && !ft_isspace(input[*i]) && j < 255)
 			delimiter[j++] = input[(*i)++];
 		delimiter[j] = '\0';
-		add_token(t_list, create_token(delimiter, TOKEN_STRING));
+		if (ft_strlen(delimiter) > 0)
+			add_token(t_list, create_token(delimiter, TOKEN_STRING));
 	}
 	else
 	{
