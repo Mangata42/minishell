23d22caa2b60a5300a13a6bd95d11ec1dd7d0547
fflamion <fflamion@student.42.fr>
2024-11-11 07:37:46 +0100

    full normed

--- src/lexer/utils2.c
+++ src/lexer/utils2.c
@@ -6,16 +6,20 @@
 /*   By: fflamion <fflamion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:09:54 by fflamion          #+#    #+#             */
-/*   Updated: 2024/11/10 17:37:06 by fflamion         ###   ########.fr       */
+/*   Updated: 2024/11/11 07:36:04 by fflamion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// Add this line at the top of your lexer.c file
-static bool	is_start_of_command = true;
+int	is_first_token(t_t_list *t_list)
+{
+	return (!t_list->first || (t_list->last && (t_list->last->type == TOKEN_PIPE
+				|| t_list->last->type == TOKEN_AND
+				|| t_list->last->type == TOKEN_OR
+				|| t_list->last->type == TOKEN_LPAREN)));
+}
 
-// Modify the h_cmd_arg function
 void	h_cmd_arg(char *input, uint16_t *i, t_t_list *t_list, char **envp)
 {
 	char	buffer[256];
@@ -23,24 +27,21 @@ void	h_cmd_arg(char *input, uint16_t *i, t_t_list *t_list, char **envp)
 
 	j = 0;
 	while (input[*i] && !ft_isspace(input[*i]) && input[*i] != '|'
-		&& input[*i] != '>' && input[*i] != '<' && j < (int)sizeof(buffer) - 1
+		&& input[*i] != '>' && input[*i] != '<' && j < (int) sizeof(buffer) - 1
 		&& input[*i] != '(' && input[*i] != ')')
 	{
 		buffer[j++] = input[(*i)++];
 	}
 	buffer[j] = '\0';
-	if (is_start_of_command)
+	if (is_first_token(t_list))
 	{
-		is_start_of_command = false;
 		if (command_exists(buffer, envp))
 			add_token(t_list, create_token(buffer, TOKEN_COMMAND));
 		else
 			add_token(t_list, create_token(buffer, TOKEN_ARGUMENT));
 	}
 	else
-	{
 		add_token(t_list, create_token(buffer, TOKEN_ARGUMENT));
-	}
 }
 
 void	handle_and(uint16_t *i, t_t_list *t_list)
--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: fflamion <fflamion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:47:28 by fflamion          #+#    #+#             */
-/*   Updated: 2024/11/11 06:20:00 by fflamion         ###   ########.fr       */
+/*   Updated: 2024/11/11 07:36:24 by fflamion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	process_input(char *input, t_sh *shell)
 	t_list = lexer(input, shell);
 	if (!t_list)
 		return (1);
-	// print_t_list(t_list);
 	if (parser(t_list))
 	{
 		free_shell(shell);
